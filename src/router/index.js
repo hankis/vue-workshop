e9261359b29c173c2718fac291d1ed147b2bc13a
Vadim Dmitrashik <34537969+hankis@users.noreply.github.com>
2020-11-11 11:19:35 +0300

    preparing to PR

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,16 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Lesson1 from '@/views/Lesson1.vue';
+// import Lesson1 from '@/views/Lesson1.vue';
 import Home from '@/views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-	{
-		path: '/l1',
-		name: 'l1',
-		component: Lesson1,
-	},
+	// {
+	// 	path: '/l1',
+	// 	name: 'l1',
+	// 	component: Lesson1,
+	// },
 	{
 		path: '/',
 		name: 'home',
